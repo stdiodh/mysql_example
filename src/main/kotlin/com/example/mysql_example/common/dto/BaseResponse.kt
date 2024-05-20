f7c76ce3c500f4cb5eee1c9d8961d4121cd4e4e0
stdiodh <playlistdh@gmail.com>
2024-05-20 11:07:27 +0900

    enum class 추가

--- src/main/kotlin/com/example/mysql_example/common/dto/BaseResponse.kt
+++ src/main/kotlin/com/example/mysql_example/common/dto/BaseResponse.kt
@@ -6,5 +6,4 @@ data class BaseResponse<T>(
     var status : String = ResultStatus.SUCCESS.name,
     var data : T? = null,
     var resultMsg : String = ResultStatus.SUCCESS.msg,
-
 )
--- src/main/kotlin/com/example/mysql_example/common/enums/ResultStatus.kt
+++ src/main/kotlin/com/example/mysql_example/common/enums/ResultStatus.kt
@@ -1,6 +1,7 @@
 package com.example.mysql_example.common.enums
 
-//이넘 클래스 사용 이유 : 개발자 입장에서 쉽게 보기 위해
+//이넘 클래스 사용 이유 : 개발자 입장에서 쉽게 보기 위해 + 유지보수 재사용성 증가
+//파라미터 안 상수 메시지 추가
 enum class ResultStatus(val msg : String) {
     SUCCESS("요청이 성공했습니다!"),
     ERROR("에러가 발생했습니다."),
